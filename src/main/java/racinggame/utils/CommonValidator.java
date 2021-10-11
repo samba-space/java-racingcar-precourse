9973aa7c64a40e778938d6880870b06b5fe1ebce
Samba <junkipark430@gmail.com>
2021-10-11 23:25:15 +0900

    refactor: null or empty 공통 validator 처리

---
+++ src/main/java/racinggame/utils/CommonValidator.java
@@ -0,0 +1,14 @@
+package racinggame.utils;
+
+import java.util.List;
+
+public class CommonValidator {
+    private CommonValidator() {
+    }
+
+    public static <T> void validateNullOrEmpty(List<T> params) {
+        if (params == null || params.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] params가 null이거나 empty입니다.");
+        }
+    }
+}
