19dd5e3d6a7a2095033da3df56990ce4d6e64b29
Samba <junkipark430@gmail.com>
2021-10-11 21:50:44 +0900

    feat: 입력에 따른 자동차 목록 이동 구현

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -12,18 +12,22 @@ public class Cars {
     }
 
     private Cars(List<Car> cars) {
-        validate(cars);
+        validateNullOrException(cars);
+        validateDuplicateCarName(cars);
         this.cars = cars;
     }
 
-    private void validate(List<Car> cars) {
-        validateNullOrException(cars);
-        validateDuplicateCarName(cars);
+    public void moveCars(List<Integer> randoms) {
+        validateNullOrException(randoms);
+        validateRandomsSize(randoms);
+        for (int i = 0; i < randoms.size(); ++i) {
+            cars.get(i).move(randoms.get(i));
+        }
     }
 
-    private void validateNullOrException(List<Car> cars) {
-        if (cars == null || cars.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] cars가 null이거나 empty입니다.");
+    private <T> void validateNullOrException(List<T> params) {
+        if (params == null || params.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] params가 null이거나 empty입니다.");
         }
     }
 
@@ -31,6 +35,12 @@ public class Cars {
         Set<Car> nonDuplicateCars = new HashSet<>(cars);
         if (cars.size() != nonDuplicateCars.size()) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름이 중복됐습니다.");
+        }
+    }
+
+    private void validateRandomsSize(List<Integer> randoms) {
+        if (cars.size() != randoms.size()) {
+            throw new IllegalArgumentException("[ERROR] randoms와 자동차 사이즈가 일치하지 않습니다.");
         }
     }
 }
--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -1,6 +1,5 @@
 package racinggame.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,27 +8,63 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CarsTest {
 
     @DisplayName("자동차 목록 중 이름이 중복되지 않을 경우 정상 생성된다.")
     @Test
     void 자동차_이름_중복_없음() {
-        Assertions.assertThatCode(() -> Cars.of(Arrays.asList(Car.ready("park"), Car.ready("abc")))).doesNotThrowAnyException();
+        assertThatCode(() -> Cars.of(Arrays.asList(Car.ready("park"), Car.ready("abc")))).doesNotThrowAnyException();
     }
 
     @DisplayName("자동차 목록 중 이름이 중복된 경우 IllegalArgumentException발생")
     @Test
     void 자동차_이름_중복_에러발생() {
-        Assertions.assertThatThrownBy(() -> Cars.of(Arrays.asList(Car.ready("park"), Car.ready("park"), Car.ready("abc"))))
+        assertThatThrownBy(() -> Cars.of(Arrays.asList(Car.ready("park"), Car.ready("park"), Car.ready("abc"))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 자동차 이름이 중복됐습니다.");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    void 자동자_목록_null_or_empty_에러발생(List<Car> cars) {
-        Assertions.assertThatThrownBy(() -> Cars.of(cars))
+    void 자동차목록_null_or_empty_에러발생(List<Car> cars) {
+        assertThatThrownBy(() -> Cars.of(cars))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] cars가 null이거나 empty입니다.");
+                .hasMessage("[ERROR] params가 null이거나 empty입니다.");
+    }
+
+    @DisplayName("자동차 목록과 randoms가 일치할 경우 자동차목록이 이동한다.")
+    @Test
+    void 자동차목록_이동() {
+        //given
+        List<Integer> randoms = Arrays.asList(1, 4, 6);
+        Cars cars = Cars.of(Arrays.asList(Car.ready("park"), Car.ready("kim"), Car.ready("song")));
+        //when, then
+        assertThatCode(() -> cars.moveCars(randoms)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("randoms가 null 또는 empty인 경우 IllegalArgumentException발생")
+    @ParameterizedTest(name = "[{index}] randoms={0}")
+    @NullAndEmptySource
+    void randoms_null_or_empty_에러발생(List<Integer> randoms) {
+        //given
+        Cars cars = Cars.of(Arrays.asList(Car.ready("park"), Car.ready("kim"), Car.ready("song")));
+        //when, then
+        assertThatThrownBy(() -> cars.moveCars(randoms))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] params가 null이거나 empty입니다.");
+    }
+
+    @DisplayName("randoms와 자동차목록의 크기가 일치하지 않는 경우 IllegalArgumentException발생")
+    @Test
+    void randoms_자동차목록_사이즈_불일치_에러발생() {
+        //given
+        List<Integer> randoms = Arrays.asList(1, 4);
+        Cars cars = Cars.of(Arrays.asList(Car.ready("park"), Car.ready("kim"), Car.ready("song")));
+        //when, then
+        assertThatThrownBy(() -> cars.moveCars(randoms))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] randoms와 자동차 사이즈가 일치하지 않습니다.");
     }
 }
