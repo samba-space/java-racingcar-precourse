5fa8f16dd691b62bc6ad6aa59410dbc16beca06f
Samba <junkipark430@gmail.com>
2021-10-13 00:47:36 +0900

    refactor: 에러처리를 위해 input return List<Car>에서 Cars로 수정

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -2,6 +2,7 @@ package racinggame.view;
 
 import nextstep.utils.Console;
 import racinggame.domain.Car;
+import racinggame.domain.Cars;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ public class InputView {
     private InputView() {
     }
 
-    public static List<Car> inputCarNames() {
+    public static Cars inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         try {
             return mapToList(Console.readLine());
@@ -31,12 +32,12 @@ public class InputView {
         }
     }
 
-    private static List<Car> mapToList(String inputCarNames) {
+    private static Cars mapToList(String inputCarNames) {
         List<Car> result = new ArrayList<>();
         String[] carNames = inputCarNames.split(",");
         for (String carName : carNames) {
             result.add(Car.ready(carName.trim()));
         }
-        return result;
+        return Cars.of(result);
     }
 }
