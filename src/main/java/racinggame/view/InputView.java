9d8e3c8a0db5b6115656578e0f5ed9fb2c1401ca
Samba <junkipark430@gmail.com>
2021-10-12 20:39:04 +0900

    feat: 사용자 입력기능

---
+++ src/main/java/racinggame/view/InputView.java
@@ -0,0 +1,41 @@
+package racinggame.view;
+
+import nextstep.utils.Console;
+import racinggame.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+    private InputView() {}
+
+    public static List<Car> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        try {
+            return mapToList(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputCarNames();
+        }
+    }
+
+    public static int inputTurn() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String count = Console.readLine();
+        try {
+            return Integer.parseInt(count);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            return inputTurn();
+        }
+    }
+
+    private static List<Car> mapToList(String inputCarNames) {
+        List<Car> result = new ArrayList<>();
+        String[] carNames = inputCarNames.split(",");
+        for (String carName : carNames) {
+            result.add(Car.ready(carName.trim()));
+        }
+        return result;
+    }
+}
